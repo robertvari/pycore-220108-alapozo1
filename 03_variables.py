ee847ead4dec47e3efe4f5dfedbcaf2dc710ced4
Robert Vári <mail.robertvari@gmail.com>
2022-01-08 16:25:27 +0100

    update

---
+++ 03_variables.py
@@ -0,0 +1,10 @@
+# define variable
+my_name = "Kiss Csaba"
+my_age = 18
+my_address = "Budapest"
+my_email = "mail.pythonsulli@gmail.com"
+
+# my_name, my_age = "Robert", 23
+
+# use, call variable
+print(my_name, my_age, my_email, my_address)
