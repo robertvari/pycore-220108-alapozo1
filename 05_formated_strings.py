0d47399b5e8073b4e0dc0cac7f31d4b9bccfca6a
Robert Vári <mail.robertvari@gmail.com>
2022-01-08 16:42:16 +0100

    update

--- 05_formated_strings.py
+++ 05_formated_strings.py
@@ -4,3 +4,6 @@ my_age = input("Your age?")
 my_email = input("You email?")
 
 print(f"Hello, my name is {my_name}. My age is {my_age}. My email is: {my_email}")
+
+# old style string formating
+print("Hello, my name is {}. My age is {}. My email is: {}".format(my_name, my_age, my_email))
