7cdf735340259c28b7f9289270a5bb2d5d8d7683
Robert Vári <mail.robertvari@gmail.com>
2022-01-08 17:42:13 +0100

    update

---
+++ 08_dictionaries.py
@@ -0,0 +1,15 @@
+phonebook = {
+    "+36 20 234 5678": {
+        "name": "Robert",
+        "address": "Budapest",
+        "email": "robert@gmail.com",
+        "age": 34,
+        "kids": 2
+    },
+    "+36 20 653 7865": {"name": "Csaba", "address": "Pécs", "email": "csaba@gmail.com", "age": 19}
+}
+
+print(phonebook["+36 20 653 7865"]["name"])
+print(phonebook["+36 20 653 7865"]["address"])
+print(phonebook["+36 20 653 7865"]["email"])
+print(phonebook["+36 20 653 7865"]["age"])
