103c25d7140caf1f1b01eb6f38e2f4a297967134
Robert Vári <mail.robertvari@gmail.com>
2022-01-08 15:19:41 +0100

    init

---
+++ 01_datatypes.py
@@ -0,0 +1,18 @@
+# booleans
+True, False
+
+# integers
+10, 100, 32332, 44345
+
+# float
+3.14, 23.456, 545.5643
+
+# strings
+'Hello World'
+"Hello World"
+"It's my job"
+
+# this is bad!
+'It's my job'
+
+# ...
