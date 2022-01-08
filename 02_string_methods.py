cb205bc25a669a63988a7c4ae762e77342d4d525
Robert Vári <mail.robertvari@gmail.com>
2022-01-08 15:46:02 +0100

    update

--- 02_string_methods.py
+++ 02_string_methods.py
@@ -6,4 +6,10 @@ print("My " + "name" + " is" + " Robert" + " Vari")
 
 print("robert vari. i live in budapest.".upper())
 print("ROBERT VARI".lower())
+print("ROBERT VARI".title())
 print("robert vari. i live in budapest.".title())
+
+# find and replace
+print("My name is Robert Vari.".replace("Robert", "Csaba"))
+print("My name is Robert Vari.".replace("Robert Vari", "Csaba Kiss"))
+print("My name is Robert Vari.".replace("a", "X"))
