c1ea8e648515b90b190b43b6d9d943644a40eac9
Robert Vári <mail.robertvari@gmail.com>
2022-01-08 16:38:45 +0100

    update

---
+++ 05_formated_strings.py
@@ -0,0 +1,6 @@
+# use input() to get data from user
+my_name = input("What is your name?")
+my_age = input("Your age?")
+my_email = input("You email?")
+
+print(f"Hello, my name is {my_name}. My age is {my_age}. My email is: {my_email}")
