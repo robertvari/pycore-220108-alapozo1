9855a976024f91e6a5aa36aa1cb667fc64c95e1d
Robert Vári <mail.robertvari@gmail.com>
2022-01-08 15:39:46 +0100

    update

---
+++ 02_string_methods.py
@@ -0,0 +1,9 @@
+print("My name is Robert Vari.")
+
+# concatenate strings
+print("Hello " + "World")
+print("My " + "name" + " is" + " Robert" + " Vari")
+
+print("robert vari. i live in budapest.".upper())
+print("ROBERT VARI".lower())
+print("robert vari. i live in budapest.".title())
