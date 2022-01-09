68a11ec11a700180d55462599d07adc624de1869
Robert Vári <mail.robertvari@gmail.com>
2022-01-09 17:12:09 +0100

    Photo to excel project

--- Photo_To_Excel/PhotoToExcel.py
+++ Photo_To_Excel/PhotoToExcel.py
@@ -1,14 +1,19 @@
 import os
 
+# open photo folder and list content
 photo_folder = r"C:\Work\_PythonSuli\pycore-220108\photos"
 files = os.listdir(photo_folder)
-photos = []
-extensions = [".jpeg", ".jpg"]
 
+
+# filter files and folders and find images (.jpeg, .jpg)
+extensions = [".jpeg", ".jpg"]
+photos = []
 for i in files:
     name, ext = os.path.splitext(i)
 
     if ext.lower() not in extensions:
         continue
 
-    print(i)
+    photos.append(i)
+
+# open images with PIL == Pillow
