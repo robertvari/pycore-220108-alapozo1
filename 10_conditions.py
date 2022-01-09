193ac84d816583571f78f9daf8a21fe46b611211
Robert Vári <mail.robertvari@gmail.com>
2022-01-09 14:43:59 +0100

    update

--- 10_conditions.py
+++ 10_conditions.py
@@ -4,6 +4,9 @@ if number == 5:
     # condition block
     print("The number is 5.")
 
+if not number == 5:
+    print("The number is not 5.")
+
 if number != 5:
     # condition block
     print("The number is not 5.")
