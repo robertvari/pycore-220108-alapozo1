99b2a131e84352186e4b789d658a90ce2cd2736b
Robert Vári <mail.robertvari@gmail.com>
2022-01-09 18:37:04 +0100

    Photo to excel project

--- Photo_To_Excel/PhotoToExcel.py
+++ Photo_To_Excel/PhotoToExcel.py
@@ -1,6 +1,8 @@
 import os
 from PIL import Image, ExifTags
 import json
+from openpyxl import Workbook
+
 
 # open photo folder and list content
 photo_folder = r"C:\Work\_PythonSuli\pycore-220108\photos"
@@ -19,9 +21,21 @@ for i in files:
     photos.append(i)
 
 # open images with PIL == Pillow
+# collect data
 photo_data = {}
 
-for file in photos:
+# open excel sheet
+workbook = Workbook()
+sheet = workbook.active
+
+sheet["A1"] = "File Path"
+sheet["B1"] = "Date"
+sheet["C1"] = "Size"
+sheet["D1"] = "Camera"
+sheet["E1"] = "ISO"
+
+
+for index, file in enumerate(photos):
     photo_path = os.path.join(photo_folder, file)
 
     img = Image.open(photo_path)
@@ -34,6 +48,10 @@ for file in photos:
         "iso": None
     }
 
+    # add data to workbook
+    sheet[f"A{index + 3}"] = photo_path
+    sheet[f"C{index + 3}"] = str(img.size)
+
     # get exif data
     exif_data = img._getexif()
     if not exif_data:
@@ -41,8 +59,16 @@ for file in photos:
 
     for key, value in exif_data.items():
         tag_name = ExifTags.TAGS.get(key)
-        print(tag_name, value)
 
+        if tag_name == "Model":
+            photo_data[file]["camera"] = value
+            sheet[f"D{index+3}"] = value
+        elif tag_name == "DateTime":
+            photo_data[file]["date"] = value
+            sheet[f"B{index + 3}"] = value
+        elif tag_name == "ISOSpeedRatings":
+            photo_data[file]["iso"] = value
+            sheet[f"E{index + 3}"] = value
 
     photo_data[file]["date"] = exif_data[306]
 
@@ -51,3 +77,7 @@ for file in photos:
 data_file_path = os.path.join(photo_folder, "photo_data.json")
 with open(data_file_path, "w") as f:
     json.dump(photo_data, f)
+
+# save excel file
+excel_file_path = os.path.join(photo_folder, "photo_data.xlsx")
+workbook.save(excel_file_path)
