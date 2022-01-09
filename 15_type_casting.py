29fd086b2792eadf61eb661022a35d9e8aa863ec
Robert Vári <mail.robertvari@gmail.com>
2022-01-09 15:52:06 +0100

    update

---
+++ 15_type_casting.py
@@ -0,0 +1,5 @@
+number1 = 5
+number2 = int("5")
+
+
+print(number1 == number2)
--- magic_number.py
+++ magic_number.py
@@ -8,3 +8,17 @@ magic_number = 5
 print("="*50, "MAGIC NUMBER", "="*50)
 print(f"There is a number between {min_number} and {max_number}. Can you guess it?")
 print(f"You have {max_tries} tries.")
+
+user_guess = input("Your guess?")
+
+while user_guess != magic_number:
+    # bad guess
+    max_tries -= 1
+
+    # check if we have tries
+    if max_tries == 0:
+        print("You have no more tries :(")
+        break
+
+    print(f"Wrong answer. You have {max_tries} tries lef.")
+    user_guess = input("Your guess?")
